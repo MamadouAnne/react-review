c0f8e6ff2040a9738c1f2a06d9d057356d5375bf
Mamadou Anne <mamadiankha@gmail.com>
2021-04-13 16:33:53 -0400

    props

--- src/index.js
+++ src/index.js
@@ -9,13 +9,20 @@ import './index.css'
     //   return <h1>Hello react developers</h1>
     
     // }
-    const title = "All the Light We Cannot See";
-    const author = "Anthony Doerr";
-    const img = "https://images.pexels.com/photos/7175547/pexels-photo-7175547.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260";
+
+const firstBook = {
+    img: "https://images.pexels.com/photos/7175547/pexels-photo-7175547.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    author: "Anthony Doerr",
+    title: "All the Light We Cannot See",
+    }
+
+    // const title = "All the Light We Cannot See";
+    // const author = "Anthony Doerr";
+    // const img = "https://images.pexels.com/photos/7175547/pexels-photo-7175547.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260";
     
     function BookList () {
-        return <section className="booklist">
-            <Book job="developer"/>
+        return <section className="booklist" >
+            <Book job="developer" img={firstBook.img} author={firstBook.author} titile={firstBook.title}/>
             <Book titile="Random title" price={12} />
 </section>
     
@@ -24,9 +31,9 @@ import './index.css'
 const Book = (props) => {
     console.log(props);
     return <article className="book">
-        <img src={img} width="200px" height="200px" alt="" />
-        <h1>{title}</h1>
-        <h4>{author}</h4>
+         <img src={props.img} width="200px" height="200px" alt="" />
+        {/* <img src={img} width="200px" height="200px" alt="" /> */}
+        <h4>{props.author}</h4>
         <p>{props.job}</p>
         <p>{props.titile} {props.price}</p>
         {/* {console.log(props)} */}
